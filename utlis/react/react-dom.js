04b5fcc40c67264553adb1071ba397deced044dd
LEVI.Y <levi.yu@sjfood.com>
2024-10-07 20:44:13 +0800

    feat: note update

--- utlis/react/react-dom.js
+++ utlis/react/react-dom.js
@@ -4,22 +4,20 @@
  * @param {container} 真实 DOM
  */
 export function render(element, container) {
-    // 处理文本节点和标签节点
-    const dom =
-        element.type == "TEXT_ELEMENT"
-            ? document.createTextNode("")
-            : document.createElement(element.type)
-    // 为节点绑定属性
-    const isProperty = key => key !== "children"
-    Object.keys(element.props)
-        .filter(isProperty)
-        .forEach(name => {
-            dom[name] = element.props[name]
-        })
-    // 处理孩子节点
-    element.props.children.forEach(child =>
-        render(child, dom)
-    )
-    // 添加到容器
-    container.appendChild(dom)
+  // 处理文本节点和标签节点
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+  // 为节点绑定属性
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+  // 处理孩子节点
+  element?.props?.children?.forEach((child) => render(child, dom));
+  // 添加到容器
+  container.appendChild(dom);
 }
